5aa920b9dc9283fe2eb189685a1e8b82cd6e5adc
Christian Suriano <cratea113@gmail.com>
2022-06-13 22:40:13 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const yellowFruit = "Banana"
+const orangeFruit = "Orange"
+const redFruit = "Apple"
+const greenFruit = "Watermelon"
+const blueFruit = "Blueberry"
+
+//shortened the list of variables by adding them into an array.
+const fruits = ["banana", "Orange", "Apple", "Watermelon", "Blueberry"] 
+
+//console logs the new array iv'e created
+console.log(fruits)
